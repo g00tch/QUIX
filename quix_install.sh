eb8ddae738b30e0bf59853c3de197eefbbe9067b
g00tch <bmatosky01@yahoo.com>
2018-07-10 21:24:32 -0700

    sdaf
    asdf

--- quix_install.sh
+++ quix_install.sh
@@ -51,7 +51,7 @@ function download_node() {
   cd $TMP_FOLDER >/dev/null 2>&1
   wget -q $COIN_TGZ
   compile_error
-  tar xvzf $COIN_ZIP >/dev/null 2>&1
+  unzip $COIN_ZIP >/dev/null 2>&1
   chmod +x $COIN_DAEMON $COIN_CLI
   cp $COIN_DAEMON $COIN_CLI $COIN_PATH
   cd ~ >/dev/null 2>&1
